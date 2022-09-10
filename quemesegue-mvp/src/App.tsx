c5f7706c7989e1bd33307c5942c6d46474418f9d
Jeanluca <jeanlucafp@gmail.com>
2022-09-09 21:34:53 -0300

    template v1 done

--- quemesegue-mvp/src/App.tsx
+++ quemesegue-mvp/src/App.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import logo from './assets/logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [insta, setInsta] = useState('')
+  const [email, setEmail] = useState('')
+  const [whats, setWhats] = useState('')
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="main-container">
+        <form onSubmit={() => {}}>
+          <img alt='logo' src={logo}></img>
+          <input value={insta} onChange={(e) => setInsta(e.target.value)} placeholder="@seuInsta"></input>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
+          <input value={whats} onChange={(e) => setWhats(e.target.value)} placeholder="Whats Apenas numeros"></input>
+          <button type="submit">Encontrar</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
   )
 }
 
