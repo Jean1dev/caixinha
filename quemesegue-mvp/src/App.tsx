9aa2277dfe8fb468a2432610ab1268454e09756e
Jeanluca <jeanlucafp@gmail.com>
2022-09-09 23:21:11 -0300

    lgpd

--- quemesegue-mvp/src/App.tsx
+++ quemesegue-mvp/src/App.tsx
@@ -6,17 +6,30 @@ function App() {
   const [insta, setInsta] = useState('')
   const [email, setEmail] = useState('')
   const [whats, setWhats] = useState('')
+  const [checked, setChecked] = useState(false)
+
+  const handleSubmit = () => {
+
+  }
 
   return (
     <div id="main-container">
-        <form onSubmit={() => {}}>
-          <img alt='logo' src={logo}></img>
-          <input value={insta} onChange={(e) => setInsta(e.target.value)} placeholder="@seuInsta"></input>
-          <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
-          <input value={whats} onChange={(e) => setWhats(e.target.value)} placeholder="Whats Apenas numeros"></input>
-          <button type="submit">Encontrar</button>
-        </form>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <img alt='logo' src={logo}></img>
+        <h3> Descubra os segredos do seu insta</h3>
+        <input value={insta} onChange={(e) => setInsta(e.target.value)} placeholder="@seuInsta"></input>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
+        <input value={whats} onChange={(e) => setWhats(e.target.value)} placeholder="Whats Apenas numeros"></input>
+
+        <div id='termos-uso'>
+          <input type={'checkbox'} checked={checked} onChange={() => setChecked(!checked)}></input>
+          <a href='https://termo-de-uso-quem-me-segue.netlify.app/' target={'_blank'}>Li e Aceito os termos de uso</a>
+        </div>
+        
+
+        <button disabled={!checked} type="submit">{checked ? 'Buscar seguidores' : 'Aceite os termos para continuar'}</button>
+      </form>
+    </div>
   )
 }
 
