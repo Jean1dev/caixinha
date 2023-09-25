c55362da38884f3b96a0c6dd14082516f56693e5
Jeanluca <jeanlucafp@gmail.com>
2023-09-25 08:53:41 -0300

    removed quem-me-segue project

--- front-end/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : "https://nodejs-backend-generic.herokuapp.com/"
-})
-
-export default api
