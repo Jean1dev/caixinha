73e1cfcaa5504b193651c418118648b959b67c7f
Jeanluca <jeanlucafp@gmail.com>
2024-04-08 11:37:30 -0300

    #107

--- caixinha-serverless/add-chave-pix/index.js
+++ caixinha-serverless/add-chave-pix/index.js
@@ -1,13 +1,26 @@
+const { resolveCircularStructureBSON } = require('../utils')
 const middleware = require('../utils/middleware')
 const { connect, getByIdOrThrow, replaceDocumentById } = require('../v2/mongo-operations')
 const { Box } = require('caixinha-core/dist/src')
 
 async function addChavePix(_context, req) {
-    const { key, url, caixinhaId } = req.body
+    const { 
+        key, 
+        url, 
+        caixinhaId,
+        replace
+    } = req.body
+
     await connect()
     const box = Box.fromJson(await getByIdOrThrow(caixinhaId))
+
+    if (replace) {
+        box['bankAccount'] = null
+    }
+    
     box.addBankAccount(key, url)
-    await replaceDocumentById(caixinhaId, 'caixinhas', box)
+
+    await replaceDocumentById(caixinhaId, 'caixinhas', resolveCircularStructureBSON(box))
 }
 
 module.exports = async (context, req) => await middleware(context, req, addChavePix)
