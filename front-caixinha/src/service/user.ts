e81b59c024b6bf5fab45c284728d0b9037a6a57e
Jeanluca <jeanlucafp@gmail.com>
2025-02-04 08:48:39 -0300

    feat: add user identification by email and username; update OrdemCompraCall to include user details

--- front-caixinha/src/service/user.ts
+++ front-caixinha/src/service/user.ts
@@ -1,3 +1,23 @@
-export function getUserId() {
-    return '64ba74538a71ec43a491d332'
+import { getUserIdByEmailAndUser } from "@/pages/api/api.carteira";
+
+interface Input {
+    email?: string;
+    username?: string;
+}
+
+export async function getUserId(input: Input): Promise<string> {
+    try {
+        const data = await getUserIdByEmailAndUser(
+            input.username,
+            input.email
+        )
+
+        return data.id
+    } catch (error) {
+        if (error instanceof Error) {
+            throw new Error(error.message);
+        } else {
+            throw new Error(String(error));
+        }
+    }
 }
