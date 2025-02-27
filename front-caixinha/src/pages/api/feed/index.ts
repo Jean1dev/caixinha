a85ef3c672249376aa1363da9d0c854dc82f8093
Jeanluca <jeanlucafp@gmail.com>
2025-02-27 14:14:26 -0300

    Update index.ts

--- front-caixinha/src/pages/api/feed/index.ts
+++ front-caixinha/src/pages/api/feed/index.ts
@@ -2,23 +2,11 @@ import { COMMUNICATION_SERVICE } from "@/constants/ApiConts"
 import state, { asyncGetWithParamethers, retornaComAtraso } from "../api.service"
 import data from "./data"
 
-const CACHE_KEY = 'feed'
-
 export async function getMeuFeed(username: string, page = 0) {
-    if (state.cache.has(CACHE_KEY)) {
-        return state.cache.get(CACHE_KEY)
-    }
-
-    let dados 
-    if (state.isDev) {
-        dados = await retornaComAtraso(data)
-    } else {
-        dados = await asyncGetWithParamethers(`https://${COMMUNICATION_SERVICE}/social-feed`, {
+    return state.isDev 
+        ? retornaComAtraso(data)
+        : asyncGetWithParamethers(`https://${COMMUNICATION_SERVICE}/social-feed`, {
             username, 
             page
         })
-    }
-
-    state.cache.set(CACHE_KEY, dados)
-    return dados
 }
